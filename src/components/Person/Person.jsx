6aab65cd4d980f2ec834d38463f5264ae1c567bd
Korshun <dmitriy.kor91@gmail.com>
2024-10-10 18:40:21 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,19 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {Object.prototype.hasOwnProperty.call(person, 'age') && (
-      <p className="Person__age">I am {person.age}</p>
-    )}
-    {!person.isMarried ? (
-      <p className="Person__partner">I am not married</p>
-    ) : person.sex === 'm' ? (
-      <p className="Person__partner">{person.partnerName} is my wife</p>
-    ) : (
-      <p className="Person__partner">{person.partnerName} is my husband</p>
-    )}
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const personMessage = !isMarried
+    ? 'I am not married'
+    : sex === 'm'
+      ? `${partnerName} is my wife`
+      : `${partnerName} is my husband`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== undefined && (
+        <p className="Person__age">I am {age}</p>
+      )}
+      <p className="Person__partner">{personMessage}</p>
+    </section>
+  );
+};
